bad5f5a89d686203130b0eb2ed7ce5b072196916
Harsh Parekh <harsh_parekh@outlook.com>
2021-07-13 15:21:29 -0400

    Enable colorful emoji.

--- config.def.h
+++ config.def.h
@@ -7,7 +7,8 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 720;                    /* -mw option; minimum width when [true]centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack Nerd Font Mono:pixelsize=12:antialias=true:autohint=true",
+	"Hack Nerd Font Mono:pixelsize=14:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=14:antialias=true:autohint=true",
 	"TerminessTTF Nerd Font Mono:pixelsize=14:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
